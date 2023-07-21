7c0bd738b5b06dbb53e54ed0f70ed95917e00731
Sonlowami <uwilowami64@outlook.com>
2023-07-21 20:31:06 +0200

    Use a decorator to increment

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,15 +2,31 @@
 """Make a query to a url and return the html"""
 import requests
 import redis
+from functools import wraps
+from typing import Callable
 
 
+def count(fn: Callable) -> Callable:
+    """Count how many times a callable has been called and return callable"""
+    @wraps(fn)
+    def counter(*args, **kwargs):
+        """count the calls to a callable and return it's value"""
+        if len(args) != 1:
+            return
+        key: str = 'count:{}'.format(args[0])
+        _redis = redis.Redis()
+        _redis.incr(key)
+        return fn(*args, **kwargs)
+    return counter
+
+
+@count
 def get_page(url: str) -> str:
     """Get a page and return it's content"""
     _redis = redis.Redis()
-    countkey: str = 'count:{}'.format(url)
-    contentkey: str = '{}'.format(url)
-    page: str = requests.get(url)
-    _redis.set(contentkey, page)
-    _redis.expire(contentkey, 10)
-    _redis.incr(countkey)
+    if _redis.get(url):
+        page: str = _redis.get(url).decode('utf-8')
+    else:
+        page: str = requests.get(url).text
+        _redis.setex(url, 10, page)
     return page
